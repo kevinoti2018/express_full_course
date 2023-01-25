a3b3353d29eff7689e0b71b067c5a0f6da412a01
kevin otieno <kelvinotix@gmail.com>
2023-01-25 22:09:01 +0300

    middlewares

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const path = require('path');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
+// cors
+// const whitelist = ['http://www.yoursite.com','http://localhost:3500','http://localhost:3500']
+// const corsOptions ={
+//     origin: (origin,callback)=>{
+//         if (whitelist.indexOf(origin) !== -1 || !origin) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     optionsSuccessStatus: 200
+// }
 
 // custom middleware
-app.use((req,res,next)=>{
-    console.log(`${req.method}  ${req.path }`);
-    next();
-})
+app.use(logger);
+// app.use(cors(corsOptions))
+
+const whiteList = ['http://localhost:3500']
+const corsOptions = {
+    origin:(origin,callback)=>{
+        //(! origin )added durig development
+        if(whiteList.indexOf(origin)==-1|| !origin){
+            callback(null,true)
+        }else{
+            callback(new Error('not defined in cors'))
+        }
+    },
+    optionsSuccessStatus:200
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.urlencoded({extended:false}))
 // middleware for json
@@ -55,8 +83,20 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('*', (req, res) => {
+    res.status(404);
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    }
+    
+      else if(req.accepts('json')){
+        res.json({error: "404 Not Found"})
+    }
+    else{
+        res.type('txt').send("404 Not Found")
+    }
+    
 })
 
+app.use(errorHandler)
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
